d3d68118df2cd031b16ebc4fc551158cb41d807c
kipkirui63 <kiprutonyachire@gmail.com>
2023-06-27 14:15:24 +0300

    Added to Fetch additional users with the same username

--- js/index.js
+++ js/index.js
@@ -21,6 +21,22 @@ form.addEventListener('submit', function(event){
         document.getElementById("user-list").innerHTML =  `
         <a target="_blank" href="https://www.github.com/${originalName}"><img src="${data.avatar_url}"></a>
     `;
+
+
+    fetch("https://api.github.com/search/users?q=" + originalName)
+    .then(response => response.json())
+    .then(searchData => {
+        var users = searchData.items;
+        for (var user of users) {
+            var listItem = document.createElement("li");
+            var link = document.createElement("a");
+            link.target = "_blank";
+            link.href = user.html_url;
+            link.textContent = user.login;
+            listItem.appendChild(link);
+            document.getElementById("user-list").appendChild(listItem);
+        }
+    });
     
         
     })
